8fed6aae570af61eb4e2f0c9bf21c80a7aa027ca
natsumi <aka.723n@gmail.com>
2022-04-19 18:14:13 +0900

    add 背景色とtop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import React from "react";
-
+import styled from 'styled-components';
+import {createGlobalStyle} from "styled-components";
 
 export const App = () => {
   return(
     <>
-      <p>a</p>
+      <GlobalStyle />
+      <Container>
+        <h2>Let's SCROLL</h2>
+        <p>JavaScriptでスクロールに合わせて背景色のCSSを変更する方法</p>
+      </Container>
+
     </>
   );
 };
 
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    height: 5000px;
+    background: #FF4081;
+    margin: 0;
+    padding: 0;
+    transition: 3s;
+  }
+  #root li{
+    list-style: none;
+  }
+
+  a{
+    text-decoration: none;
+    color: #666;
+  }
+  img{
+    width: 100%;
+  }
+
+`;
+
+
+const Container = styled.div`
+
+  h2 {
+    position: fixed;
+    top: 0;
+    padding: 10px;
+    color: black;
+    font-size: 4rem;
+  }
+  
+`;
