00b678e98f6d09f8c05b5a1da68e4065e5cd8056
natsumi <aka.723n@gmail.com>
2022-04-20 10:17:44 +0900

    add: headerのWORKS、SKILLS、PROFILE追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,32 @@
 import sampleImg from "../assets/images/sample1.png";
+import styled from 'styled-components';
 
 
 
 export const Header=()=>{
 
   return(
-    <div>
+    <HeaderTop>
       <img src={sampleImg} alt=""/>
-
-    </div>
+      <ul>
+        <li>WORKS</li>
+        <li>SKILLS</li>
+        <li>PROFILE</li>
+      </ul>
+    </HeaderTop>
 
   );
 
 }
+
+const HeaderTop = styled.div`
+  display: flex;
+  justify-content: space-between;
+  img{
+    width: 5%;
+  }
+  ul{
+    display: flex;
+  }
+  
+`;
