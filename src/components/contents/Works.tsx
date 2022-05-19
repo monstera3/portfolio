a81b24976f4b6994511876d6058f4479a55d70e5
natsumi <aka.723n@gmail.com>
2022-05-19 14:16:13 +0900

    feat: add card

--- src/components/contents/Works.tsx
+++ src/components/contents/Works.tsx
@@ -1,4 +1,4 @@
-
+import sample2Img from '../../assets/images/sample2.jpg';
 import styled from 'styled-components';
 
 
@@ -7,11 +7,13 @@ export const Works=()=>{
 
   return(
     <WorksAll id="works">
-      <div className="work-card">
 
-      </div>
       <h2  className="category">WORKS</h2>
 
+      <div className="work-card">
+        <img src={sample2Img} alt=""/>
+        <title>タイトル</title>
+      </div>
 
     </WorksAll>
 
@@ -23,8 +25,20 @@ const WorksAll = styled.div`
   max-width: 960px;
   margin: 0 auto;
   height: 50rem;
+  
   .category{
     text-align: left;
     font-size: 2rem;
+  }
+  .work-card{
+    background-color: white;
+    width: 30rem;
+    height: 30em;
+    border-radius: 8px;
+    img{
+      width: 100%;
+      height: 20em;
+      border-radius: 8px;
+    }
   }
 `;
